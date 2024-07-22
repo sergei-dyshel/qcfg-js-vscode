d6707d59650fcd9fdecc8c2502d1f8ce9c0a40ff
Sergei Dyshel <sergei@dyshel.com>
2024-07-22 09:52:33 +0300

    +

---
+++ src/mock-vscode.ts
@@ -0,0 +1,72 @@
+/* eslint-disable @typescript-eslint/no-extraneous-class */
+export class EventEmitter {}
+
+export class TreeItem {}
+
+export class ThemeIcon {}
+
+export class ThemeColor {}
+
+export enum TreeItemCollapsibleState {
+	EXPANDED = 0,
+}
+
+export namespace workspace {
+	export const fs = undefined;
+}
+
+export class Uri {}
+
+export class window {}
+
+export class tasks {}
+
+export class commands {}
+
+export class ProcessExecution {}
+
+export class ShellExecution {}
+
+export class Task {}
+
+export class TaskRevealKind {}
+
+export class TaskScope {}
+
+export class TabInputText {}
+
+export class TabInputTextDiff {}
+
+export class ProgressLocation {}
+
+export class Disposable {}
+
+export class FileType {}
+
+export class DataTransferItem {}
+
+export class FileDecoration {}
+
+export class MarkdownString {}
+
+export class env {}
+
+export class InputBoxValidationSeverity {}
+
+export class FileChangeType {}
+
+export class StatusBarAlignment {}
+
+export class TextEditorRevealType {}
+
+export class CommentThreadState {}
+
+export class CommentThreadCollapsibleState {}
+
+export class Range {}
+
+export class CommentMode {}
+
+export class comments {}
+
+export class ExtensionMode {}
