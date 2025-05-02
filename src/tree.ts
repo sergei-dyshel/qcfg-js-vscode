45e458a837c19d74a59658f0cb6db8d0bf473d99
Sergei Dyshel <sergei@dyshel.com>
2025-05-02 22:33:28 +0300

    tree: add support for checkboxes, refactor to remove vscode namespace

--- src/tree.ts
+++ src/tree.ts
@@ -1,11 +1,29 @@
-import { InstanceLogger, ModuleLogger, type Logger } from "@sergei-dyshel/node/logging";
+import { InstanceLogger, type Logger, ModuleLogger } from "@sergei-dyshel/node/logging";
 import { DisposableContainer } from "@sergei-dyshel/typescript";
 import { mapAsync, normalizeArray } from "@sergei-dyshel/typescript/array";
 import * as fail from "@sergei-dyshel/typescript/error";
 import { assert } from "@sergei-dyshel/typescript/error";
 import { objectGetOrSetProperty } from "@sergei-dyshel/typescript/object";
 import type { Arrayable, Awaitable } from "@sergei-dyshel/typescript/types";
-import * as vscode from "vscode";
+import {
+  type CancellationToken,
+  type Command,
+  type DataTransfer,
+  DataTransferItem,
+  EventEmitter,
+  type FileDecoration,
+  type MarkdownString,
+  type TreeCheckboxChangeEvent,
+  type TreeDataProvider,
+  type TreeDragAndDropController,
+  type TreeItem,
+  TreeItemCheckboxState,
+  TreeItemCollapsibleState,
+  type TreeView,
+  type TreeViewSelectionChangeEvent,
+  type Uri,
+  window,
+} from "vscode";
 import { libraryLogger } from "./common";
 import { reportErrorsAndRethrow, reportErrorsNoRethrow } from "./error-handling";
 import { Commands } from "./namespaces/commands";
@@ -19,12 +37,23 @@ const logger = new ModuleLogger({ parent: libraryLogger });
  * For better type safety can be parametrized by node type (but not required).
  */
 export interface TreeNode<T = unknown> {
-  /** Same as {@link vscode.TreeDataProvider.getChildren} */
+  /** Same as {@link TreeDataProvider.getChildren} */
   getChildren?(): Awaitable<T[]> | undefined;
-  /** Same as {@link vscode.TreeDataProvider.getTreeItem} */
-  getTreeItem(): Awaitable<vscode.TreeItem>;
+  /**
+   * Same as {@link TreeDataProvider.getTreeItem}
+   *
+   * Should not provide {@link TreeItem.checkboxState}, instead use {@link TreeNode.getChecked}
+   */
+  getTreeItem(): Awaitable<TreeItem>;
   /** Called when node is selected */
   onSelected?(): void;
+  /**
+   * Called together with {@link TreeNode.getTreeItem getTreeItem} to augment TreeItem with checkbox
+   * state
+   */
+  getChecked?(): Awaitable<boolean>;
+  /** Called when node checkbox state changes */
+  onChecked?(checked: boolean): void;
   /** Called when node starts dragging, returns whether node can be dragged */
   onDrag?(): boolean;
   /**
@@ -34,48 +63,46 @@ export interface TreeNode<T = unknown> {
    */
   onDrop?(source: T): void | Promise<void>;
   /** @returns Object where keys are decoration names provided in {@link Tree.createView} */
-  getDecorations?(): Record<string, vscode.FileDecoration | undefined> | undefined;
+  getDecorations?(): Record<string, FileDecoration | undefined> | undefined;
   /**
-   * When parent node provides {@link vscode.TreeItem.resourceUri}, construct this node's resourceUri
-   * by appending these segments to path.
+   * When parent node provides {@link TreeItem.resourceUri}, construct this node's resourceUri by
+   * appending these segments to path.
    *
-   * NOTE: when defined, {@link vscode.TreeItem.resourceUri} returned {@link TreeNode.getTreeItem} for
-   * this node should be undefined
+   * NOTE: when defined, {@link TreeItem.resourceUri} returned {@link TreeNode.getTreeItem} for this
+   * node should be undefined
    */
   // REFACTOR: make this a property
   getUriPathSegments?(): Arrayable<string>;
   /**
    * Provide tooltip/command later when ${@link Tree.getTreeItem} returned tree item without them.
    *
-   * See {@link vscode.TreeDataProvider.resolveTreeItem} for further explanation.
+   * See {@link TreeDataProvider.resolveTreeItem} for further explanation.
    */
   resolveTreeItem?(): Awaitable<{
-    tooltip?: string | vscode.MarkdownString;
-    command?: vscode.Command;
+    tooltip?: string | MarkdownString;
+    command?: Command;
   }>;
 }
 
 export class Tree<T extends TreeNode<T>>
   extends DisposableContainer
-  implements vscode.TreeDataProvider<T>, vscode.TreeDragAndDropController<T>
+  implements TreeDataProvider<T>, TreeDragAndDropController<T>
 {
   logger!: Logger;
 
   /** Initialized with {@link Tree.createView } */
-  treeView!: vscode.TreeView<T>;
+  treeView!: TreeView<T>;
   viewId!: string;
 
   // eslint-disable-next-line @typescript-eslint/no-invalid-void-type
-  private readonly onChangeEmitter = new vscode.EventEmitter<T | T[] | undefined | void>();
+  private readonly onChangeEmitter = new EventEmitter<T | T[] | undefined | void>();
   private data: T[] = [];
   private promise?: Promise<T[]>;
 
-  protected decorationChangeEmitter = new vscode.EventEmitter<
-    vscode.Uri | vscode.Uri[] | undefined
-  >();
+  protected decorationChangeEmitter = new EventEmitter<Uri | Uri[] | undefined>();
 
-  static readonly EXPANDED = vscode.TreeItemCollapsibleState.Expanded;
-  static readonly COLLAPSED = vscode.TreeItemCollapsibleState.Collapsed;
+  static readonly EXPANDED = TreeItemCollapsibleState.Expanded;
+  static readonly COLLAPSED = TreeItemCollapsibleState.Collapsed;
 
   getData() {
     return this.data;
@@ -84,28 +111,33 @@ export class Tree<T extends TreeNode<T>>
   createView(viewId: string, options?: { allowDragAndDrop?: boolean; decorationNames?: string[] }) {
     this.viewId = viewId;
     this.logger = new InstanceLogger(viewId, { parent: logger });
-    this.treeView = vscode.window.createTreeView<T>(viewId, {
+    this.treeView = window.createTreeView<T>(viewId, {
       treeDataProvider: this,
       dragAndDropController: options?.allowDragAndDrop ? this : undefined,
     });
     this.treeView.onDidChangeSelection(
-      reportErrorsNoRethrow((event: vscode.TreeViewSelectionChangeEvent<T>) =>
+      reportErrorsNoRethrow((event: TreeViewSelectionChangeEvent<T>) =>
         this.onDidChangeSelection(event),
+      ),
+    );
+    this.treeView.onDidChangeCheckboxState(
+      reportErrorsNoRethrow((event: TreeCheckboxChangeEvent<T>) =>
+        this.onDidChangeCheckboxState(event),
       ),
     );
     this.registerDisposable(this.treeView);
     if (options?.decorationNames) {
       this.registerDisposable(
         ...options.decorationNames.map((name) =>
-          vscode.window.registerFileDecorationProvider({
+          window.registerFileDecorationProvider({
             onDidChangeFileDecorations: this.decorationChangeEmitter.event,
-            provideFileDecoration: (uri: vscode.Uri, _token: vscode.CancellationToken) => {
+            provideFileDecoration: (uri: Uri, _token: CancellationToken) => {
               const node = this.findFirstNode((node) => {
-                const resourceUri = Tree.getMetadata(node).treeItem?.resourceUri;
+                const resourceUri = this.getMetadata(node).treeItem?.resourceUri;
                 return resourceUri ? URI.equals(uri, resourceUri) : false;
               });
               if (!node) return;
-              const metadata = Tree.getMetadata(node);
+              const metadata = this.getMetadata(node);
               if (!metadata.decorations && node.getDecorations) {
                 metadata.decorations = node.getDecorations();
                 for (const name in metadata.decorations)
@@ -157,7 +189,7 @@ export class Tree<T extends TreeNode<T>>
   }
 
   updateDecorations(node: T, propogateToParent = false) {
-    const metadata = Tree.getMetadata(node);
+    const metadata = this.getMetadata(node);
     fail.assertNotNull(
       !metadata.treeItem,
       "Can not update decorations on node which have not called getTreeItem yet",
@@ -178,7 +210,7 @@ export class Tree<T extends TreeNode<T>>
       return;
     }
     yield root;
-    for (const child of Tree.getMetadata(root).children ?? [])
+    for (const child of this.getMetadata(root).children ?? [])
       for (const node of this.iterateNodes(child)) yield node;
   }
 
@@ -187,8 +219,12 @@ export class Tree<T extends TreeNode<T>>
     return undefined;
   }
 
+  static getTree<T extends TreeNode<T>>(node: T) {
+    return this.getMetadata(node).tree;
+  }
+
   private clearMetadata(node: T, recursive: boolean) {
-    const metadata = Tree.getMetadata(node);
+    const metadata = this.getMetadata(node);
     if (recursive && metadata.children)
       metadata.children.forEach((child) => this.clearMetadata(child, recursive));
     metadata.treeItem = undefined;
@@ -196,18 +232,31 @@ export class Tree<T extends TreeNode<T>>
     metadata.decorations = undefined;
   }
 
-  private static getMetadata<T>(node: T) {
+  private getMetadata(node: T) {
     // the idiomatic way here is to use WeakMap
     // but embedding metadata in the node makes debugging easier
-    return objectGetOrSetProperty(node, "__tree", () => ({}) as TreeNodeMetadata<T>);
+    return objectGetOrSetProperty(node, "__tree", () => new TreeNodeMetadata<T>(this));
   }
 
-  private onDidChangeSelection(event: vscode.TreeViewSelectionChangeEvent<T>) {
+  private static getMetadata<T extends TreeNode<T>>(node: T) {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    const metadata = (node as any).__tree as TreeNodeMetadata<T>;
+    fail.assertNotNull(metadata, "Node does not have metadata yet");
+    return metadata;
+  }
+
+  private onDidChangeSelection(event: TreeViewSelectionChangeEvent<T>) {
     for (const selected of event.selection) if (selected.onSelected) selected.onSelected();
   }
 
-  static getResourceUri<T extends TreeNode<T>>(node: T) {
-    return Tree.getMetadata(node).treeItem?.resourceUri;
+  private onDidChangeCheckboxState(event: TreeCheckboxChangeEvent<T>) {
+    for (const [node, state] of event.items) {
+      node.onChecked?.(state === TreeItemCheckboxState.Checked);
+    }
+  }
+
+  getResourceUri(node: T) {
+    return this.getMetadata(node).treeItem?.resourceUri;
   }
 
   /*
@@ -218,9 +267,14 @@ export class Tree<T extends TreeNode<T>>
   getTreeItem(node: T) {
     return reportErrorsAndRethrow(
       async () => {
-        const metadata = Tree.getMetadata(node);
+        const metadata = this.getMetadata(node);
         if (metadata.treeItem) return metadata.treeItem;
         const treeItem = await node.getTreeItem();
+        // this will overwrite checkboxState if it was provided by getTreeItem
+        if (node.getChecked)
+          treeItem.checkboxState = (await node.getChecked())
+            ? TreeItemCheckboxState.Checked
+            : TreeItemCheckboxState.Unchecked;
         // if available, copy static property viewItemId from node class so that
         // commands defined with decorator would work (the "when" clause)
         if (!treeItem.contextValue) {
@@ -230,11 +284,13 @@ export class Tree<T extends TreeNode<T>>
             assert(
               treeItem.contextValue === undefined,
               "Can not override `contextValue` when `viewItemId` is defined by tree view item class",
+              treeItem,
             );
             treeItem.contextValue = metadata.viewItemId;
           }
         }
 
+        const parent = metadata.parent ? this.getMetadata(metadata.parent).treeItem : undefined;
         const pathSegments = normalizeArray(node.getUriPathSegments?.());
         if (pathSegments.length > 0) {
           assert(
@@ -247,7 +303,7 @@ export class Tree<T extends TreeNode<T>>
             `Node only defines path segments but has no parent`,
             node,
           );
-          const parentUri = Tree.getMetadata(metadata.parent).treeItem?.resourceUri;
+          const parentUri = parent?.resourceUri;
           fail.assertNotNull(
             parentUri,
             `Node only defines path segments but parent node does not provide resourceUri`,
@@ -277,7 +333,7 @@ export class Tree<T extends TreeNode<T>>
 
   async getChildren(node?: T) {
     if (node) {
-      const metadata = Tree.getMetadata(node);
+      const metadata = this.getMetadata(node);
       if (metadata.children) return metadata.children;
     }
 
@@ -289,10 +345,10 @@ export class Tree<T extends TreeNode<T>>
           }
           return this.data;
         }
-        const metadata = Tree.getMetadata(node);
+        const metadata = this.getMetadata(node);
         metadata.children = node.getChildren ? await node.getChildren() : undefined;
         if (metadata.children)
-          for (const child of metadata.children) Tree.getMetadata(child).parent = node;
+          for (const child of metadata.children) this.getMetadata(child).parent = node;
         return metadata.children;
       },
       { prefix: "getChildren failed: " },
@@ -300,10 +356,10 @@ export class Tree<T extends TreeNode<T>>
   }
 
   getParent(node: T): T | undefined {
-    return Tree.getMetadata(node).parent;
+    return this.getMetadata(node).parent;
   }
 
-  async resolveTreeItem(treeItem: vscode.TreeItem, node: T, _: vscode.CancellationToken) {
+  async resolveTreeItem(treeItem: TreeItem, node: T, _: CancellationToken) {
     if (!node.resolveTreeItem) return undefined;
     const resolved = await node.resolveTreeItem();
     // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
@@ -331,40 +387,32 @@ export class Tree<T extends TreeNode<T>>
     return [this.mimeType];
   }
 
-  handleDrag(
-    sources: readonly T[],
-    dataTransfer: vscode.DataTransfer,
-    _token: vscode.CancellationToken,
-  ) {
+  handleDrag(sources: readonly T[], dataTransfer: DataTransfer, _token: CancellationToken) {
     reportErrorsNoRethrow(() => {
       assert(sources.length === 1, "Multi-select not supported");
       const node = sources[0];
       if (!node.onDrag) return;
       if (!node.onDrag()) return;
-      const md = Tree.getMetadata(node);
+      const md = this.getMetadata(node);
       fail.assertNotNull(md.treeItem);
       fail.assertNotNull(md.treeItem.resourceUri, "Dragged node has no resourceUri");
       this.logger.debug(`Dragging node`, sources[0], node);
       // DataTrasfer values must be JSON objects
       dataTransfer.set(
         this.mimeType,
-        new vscode.DataTransferItem(md.treeItem.resourceUri.toString(true /* skipEncoding */)),
+        new DataTransferItem(md.treeItem.resourceUri.toString(true /* skipEncoding */)),
       );
     })();
   }
 
-  async handleDrop(
-    target: T | undefined,
-    dataTransfer: vscode.DataTransfer,
-    _token: vscode.CancellationToken,
-  ) {
+  async handleDrop(target: T | undefined, dataTransfer: DataTransfer, _token: CancellationToken) {
     return reportErrorsNoRethrow(async () => {
       const dtItem = dataTransfer.get(this.mimeType);
       fail.assertNotNull(dtItem, `No DataTransferItem for tree's MIME type`);
       const sourceUri = dtItem.value as string;
       const source = this.findFirstNode(
         (node) =>
-          Tree.getMetadata(node).treeItem?.resourceUri?.toString(true /* skipEncoding */) ===
+          this.getMetadata(node).treeItem?.resourceUri?.toString(true /* skipEncoding */) ===
           sourceUri,
       );
       fail.assertNotNull(source, "Can not find dragged node with uri", sourceUri);
@@ -382,14 +430,10 @@ export class Tree<T extends TreeNode<T>>
   }
 }
 
-class TreeNodeMetadata<T> {
-  treeItem?: vscode.TreeItem;
+class TreeNodeMetadata<T extends TreeNode<T>> {
+  constructor(readonly tree: Tree<T>) {}
+  treeItem?: TreeItem;
   children?: T[];
   parent?: T;
-  decorations?: Record<string, vscode.FileDecoration | undefined> | undefined;
+  decorations?: Record<string, FileDecoration | undefined> | undefined;
 }
-
-// interface NodeUriDtValue {
-// 	__type: 'uri';
-// 	uri: string;
-// }
