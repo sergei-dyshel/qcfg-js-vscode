9ea2bf849a40e2344303f87616ac48182ebd80c5
Sergei Dyshel <sergei@amazon.com>
2025-07-26 11:11:37 -0700

    add "resolvers" proposed API

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,4 +2,4 @@
 
 import myconfig from "@sergei-dyshel/eslint-config";
 
-export default [...myconfig, { ignores: ["mock-register.js"] }];
+export default [...myconfig, { ignores: ["mock-register.js", "vscode.proposed.*.d.ts"] }];
